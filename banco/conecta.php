e5ede2d75170f454ccdbc8f7dd1e71f2c1984a00
VictorDS27 <victor.vieira2704@gmail.com>
2022-11-01 11:24:03 -0300

    acho que ta feito

--- banco/conecta.php
+++ banco/conecta.php
@@ -1,6 +1,9 @@
 <?php
     try{
         $conexao = mysqli_connect("localhost","root","","bd_vulneravel");
+
+
+        // uma vulnerabilidade é que o banco de dados não tem senha, isso em uma hospedagem não é nada legal kkkkkkk
         
     }catch(Exception $e){
         echo "Erro: $e";
